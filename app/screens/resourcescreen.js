db752814bf04f16ff0331edc9bd733010358488f
Lu Wang <wangl33@uw.edu>
2020-11-10 22:28:56 -0800

    update resource page-sort view

--- app/screens/resourcescreen.js
+++ app/screens/resourcescreen.js
@@ -1,132 +1,150 @@
-import React, { Component } from 'react';
-import {StyleSheet, View, Text, Button, ImageBackground, RefreshControlComponent, ScrollView} from 'react-native';
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, TouchableOpacity, ScrollView } from "react-native";
+import RNPickerSelect from 'react-native-picker-select';
 import AppHeader from "../components/AppHeader/AppHeader";
 import { tabsHeader } from "../assets/styles";
 import LeftHeader from "../components/AppHeader/LeftHeader";
 import SOSButton from "../components/HeaderComponents/SOSButton";
-import { color } from '../assets/constant';
-import { useNavigation } from "@react-navigation/native";
+import SVGIcon from '../components/SVGIcon/SVGIcon';
 import ResourcesContainer from "../components/ResourcesComponent/ResourcesContainer";
-import ResourceCard from "../components/ResourcesComponent/ResourceCard";
-import ExploreAllContainer from "../components/ResourcesComponent/ExploreAll/ExploreAllContainer";
-import ExploreCard from "../components/ResourcesComponent/ExploreAll/ExploreCard";
+import ResourceListCard from '../components/ResourcesComponent/ResourceListCard';
+import { IMAGE_SOURCE } from '../constant';
+import searchIcon from '../assets/icons/search-icon';
+import pickerIcon from '../assets/icons/picker-icon';
+import { useNavigation } from "@react-navigation/native";
 
-const EMERGENCY_CARD = [
-  {
-    category: "Self Harm",
-    backgroundColor: "#D4F2F9",
-    color: "#C5E4EC"
-  },
-  {
-    category: "Domestic Violence",
-    backgroundColor: "#FCE4EC",
-    color: "#FBC6C0"
-  },
-  {
-    category: "Self Harm",
-    backgroundColor: "#D4F2F9",
-    color: "#C5E4EC"
-  },
-  {
-    category: "COVID 19",
-    backgroundColor: "#D4F7DA",
-    color: "#C1EEC9"
-  },
-];
 
-export const ResourceScreen = (props) => {
-  return (
-      <View style={{flex: 1}}>
-          <AppHeader
-            leftComponent={() => <LeftHeader/>}
-            rightComponent={<SOSButton/>}
-            headerStyle={{...tabsHeader}}
+export const ResourceScreen = (props) =>  {
+
+  const [sortBy, setSortBy] = useState("name");
+  const navigation = useNavigation();
+
+  const data = {
+    Resources: [
+      {
+        id: 0,
+        title: "Want to Stay Hydrated? Drink Before You’re Thirsty",
+        category: "SymptomManagement",
+        about: ["ANXIOUS"],
+        image: "https://images.unsplash.com/photo-1584444262846-e2716db1294b",
+        favored: false,
+      },
+      {
+        id: 1,
+        title: "Caregiver Stress and Burnout",
+        category: "SymptomManagement",
+        about: "TIRED",
+        image: "https://images.unsplash.com/photo-1584444262846-e2716db1294b",
+        favored: false,
+      },
+      {
+        id: 2,
+        title: "Want to Stay Hydrated? Drink Before You’re Thirsty",
+        category: "SymptomManagement",
+        about: "health",
+        image: "https://images.unsplash.com/photo-1584444262846-e2716db1294b",
+        favored: false,
+      },
+    ],
+  };
+
+  const renderResources = () => {
+    return data.Resources.map((resource) => {
+        return (
+        <ResourceListCard 
+          key={resource.id}
+          text={resource.title}
+          src="coco testing"
+          label={[
+            {
+              category: resource.category,
+              text: resource.about,
+            },
+            ]}
+          resourceImage={resource.image}
           />
-          <ScrollView style={{flex: 1}}>
-            <Text style={styles.HeaderStyle}>Resources</Text>
-            <ResourcesContainer title="Favorites">
-              <ResourceCard
-                text="Want to Stay Hydrated? Drink Before You’re Thirsty"
-                label={[{
-                  category: "SymptomManagement",
-                  text: "health"
-                }]} resourceImage="https://images.unsplash.com/photo-1584444262846-e2716db1294b" />
-              <ResourceCard
-                text="Want to Stay Hydrated? Drink Before You’re Thirsty"
-                label={[{
-                  category: "SymptomManagement",
-                  text: "health"
-                }]} resourceImage="https://images.unsplash.com/photo-1584444262846-e2716db1294b" />
-            </ResourcesContainer>
-            <ResourcesContainer title="Suggested">
-              <ResourceCard
-                text="Want to Stay Hydrated? Drink Before You’re Thirsty"
-                label={[{
-                  category: "SymptomManagement",
-                  text: "health"
-                }]} resourceImage="https://images.unsplash.com/photo-1584444262846-e2716db1294b" />
-              <ResourceCard
-                text="Want to Stay Hydrated? Drink Before You’re Thirsty"
-                label={[{
-                  category: "SymptomManagement",
-                  text: "health"
-                }]} resourceImage="https://images.unsplash.com/photo-1584444262846-e2716db1294b" />
-            </ResourcesContainer>
-            <ExploreAllContainer>
-            </ExploreAllContainer>
-          </ScrollView>
+
+        );
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+      <AppHeader
+        leftComponent={() => <LeftHeader/>}
+        rightComponent={<SOSButton/>}
+        headerStyle={{...tabsHeader, position: "absolute", right: 0, left: 0}}
+      />
+      <View style={styles.searchBar}>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Text style={styles.textStyle}>SORT BY:</Text>
+          <View style={styles.pickerContainerStyle}>
+            <RNPickerSelect
+              value={sortBy}
+              onValueChange={(value) => setSortBy(value)}
+              Icon={() => <View style={{top: 5, right: 5}}><SVGIcon height="14" width="14" src={pickerIcon} /></View>}
+              placeholder={{}}
+              style={{
+                inputIOS: {
+                  top: 2,
+                  fontSize: 12,
+                  fontFamily: "Poppins-Regular",
+                  color: "rgba(0, 0, 0, 0.65)"
+                },
+                inputAndroid: {
+                  top: 1,
+                  fontSize: 12,
+                  fontFamily: "Poppins-Regular",
+                  color: "rgba(0, 0, 0, 0.65)"
+                },
+              }}
+              useNativeAndroidPickerStyle={false}
+              items={[
+                { label: 'Name', value: 'name' },
+                { label: 'Newest', value: 'newest' },
+              ]}
+          />
+          </View>
+        </View>
       </View>
-    )
-}
+      <ScrollView style={{flex: 1}}>
+        {renderResources()}
+      </ScrollView>
+  </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  HeaderStyle: {
-    fontFamily: 'Poppins-Bold',
-    paddingLeft: 25,
-    fontSize: 24,
-    color: "#6D6A6A"
+  container: {
+    backgroundColor: 'white',
+    height: '100%'
   },
-  text: {
-    fontSize: 30
-  },
-  contactContainer: {
-    alignItems: "center",
-    marginVertical: 30,
-    paddingHorizontal: "15%",
-  },
-  contactButton: {
+  searchBar: {
+    paddingBottom: 10,
+    borderBottomWidth: 2,
+    borderBottomColor: "#D8DEFF",
+    paddingTop: 80,
+    paddingBottom: 10,
+    marginTop: 20,
+    marginLeft: 25,
+    marginRight: 25,
     flexDirection: "row",
-    backgroundColor: "#F1F3FE",
-    borderRadius: 20,
-    width: "100%",
-    paddingVertical: 10,
-    paddingHorizontal: 25,
-    marginBottom: 8,
-    padding: 5,
-  },
-  ButtonTextContainer: {
-    flex: 1,
+    justifyContent: "space-between",
     alignItems: "center"
   },
-  exploreListContainer: {
-    marginHorizontal: 25,
-    marginVertical: 15,
-  },
-  exploreTitleStyle: {
-    fontFamily: "Poppins-Medium",
-    fontSize: 16,
-    marginBottom: 5,
-    color: "#454545"
-  },
-  exploreList: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-between"
-  },
-  linkText: {
-    fontFamily: "Poppins-Regular",
+  textStyle: {
     fontSize: 12,
-    color: "#FF786A",
-    textDecorationLine: "underline"
-  }
+    fontFamily: "Poppins-Medium",
+    color: "#686868",
+    textTransform: "uppercase"
+  },
+  pickerContainerStyle: {
+    borderWidth: 1,
+    paddingLeft: 5,
+    width: 90,
+    height: 24,
+    marginLeft: 8,
+    borderRadius: 4,
+    borderColor: "#D9D9D9"
+  },
 })
